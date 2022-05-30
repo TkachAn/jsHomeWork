1316d4e560fee76346ff5c165ad425f0b6d3c6b9
TkachAn <4ndrey.tkachenko@gmail.com>
2022-05-30 15:51:53 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,5 +1,8 @@
 const inputText = document.querySelector("#name-input");
 inputText.addEventListener("input", onInputChange);
 function onInputChange(event){
-    document.querySelector("#name-output").textContent = event.currentTarget.value;
+	if(event.currentTarget.value !==''){
+	document.querySelector("#name-output").textContent = event.currentTarget.value;
+	}else{
+	document.querySelector("#name-output").textContent = "Anonymous";}
 }
